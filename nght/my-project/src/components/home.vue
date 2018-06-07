55824d9e2682b5b428a7c5812b9d21630277f2f6
youngpj <792796905@qq.com>
2018-06-07 17:26:31 +0800

    '666'

--- nght/my-project/src/components/home.vue
+++ nght/my-project/src/components/home.vue
@@ -41,7 +41,7 @@
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
+            <el-button>取消666</el-button>
         </el-form-item>
         <div>{{isLogin}}</div>
     </el-form>
